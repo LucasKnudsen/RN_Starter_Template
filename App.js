3774fe33913f226b5cf61ac11966479d65eef68d
Lucas <laksteelhouse@gmail.com>
2022-06-12 18:44:54 +0200

    Additional dependencies

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { SafeAreaView, StatusBar, Text, TouchableOpacity } from 'react-native'
 import Toast from 'react-native-toast-message'
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 import { FONTS } from './src/styles/theme'
 import { toast } from './src/services'
 
@@ -11,6 +13,8 @@ const App = () => {
       <Text style={{ ...FONTS.largeTitle, textAlign: 'center' }}>
         HELLO STARTER TEMPLATE
       </Text>
+
+      <Icon name='rocket' size={30} color='#900' />
 
       <TouchableOpacity onPress={() => toast.success('Welcome to the starter template.')}>
         <Text>Hey</Text>
--- src/navigation/main/MainStack.js
+++ src/navigation/main/MainStack.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const MainStack = () => {
-  return <NavigationContainer></NavigationContainer>
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer></NavigationContainer>
+    </SafeAreaProvider>
+  )
 }
 
 export default MainStack
